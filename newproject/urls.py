53c156b63afae7dd939351329b530fb5121e20f4
indian9361072610 <111270826+indian9361072610@users.noreply.github.com>
2022-09-26 00:04:55 +0530

    Update urls.py

--- newproject/urls.py
+++ newproject/urls.py
@@ -35,9 +35,9 @@ urlpatterns = [
 ]
 
 # urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
    
 
